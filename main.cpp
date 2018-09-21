3b7e7b221b9c4ec354e7d602eb7a241211ce60dd
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-09-21 17:43:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,19 +6,25 @@ using json = nlohmann::json;
 
 int main() {
 	json json_data = R"(
-{
-    "lastname" : "Ivanov",
-    "firstname" : "Ivan",
-    "age" : 25,
-    "islegal" : false,
-    "marks" : [
-    	4,5,5,5,2,3
-    ],
-    "address" : {
-    	"city" : "Moscow",
-        "street" : "Vozdvijenka"
-    }
-})"_json;
+[
+    ["Si-9.15", "RTS-9.15", "GAZP-9.15"],
+    [100024, 100027, 100050],
+    ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
+
+])"_json;
+	json j_0 = json_data[0];
+	json j_1 = json_data[1];
+	json j_2 = json_data[2];
 	
-	std::cout << json_data << std::endl;
+	json json_data_converte;
+	
+	for (unsigned int i = 0; i < 3; i++){
+		json jj;
+		jj["ticker"] = j_0[i];
+		jj["id"] = j_1[i];
+		jj["description"] = j_2[i];
+		json_data_converte.push_back(jj);
+	}
+	
+	std::cout << json_data_converte << std::endl;
 }
