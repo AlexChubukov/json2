93aedc4516eb94b146e9048b5e358767fc37d34b
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-09-21 18:06:29 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 #include "json.hpp"
-#include <nlohmann/json.hpp>
 
 namespace {
 
@@ -34,30 +33,4 @@ TEST(Json, LoadFromString) {
   EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
 
-}
-
-using json = nlohmann::json;
-
-int main() {
-	json json_data = R"(
-[
-    ["Si-9.15", "RTS-9.15", "GAZP-9.15"],
-    [100024, 100027, 100050],
-    ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
-])"_json;
-	json j_0 = json_data[0];
-	json j_1 = json_data[1];
-	json j_2 = json_data[2];
-	
-	json json_data_converte;
-	
-	for (unsigned int i = 0; i < 3; i++){
-		json jj;
-		jj["ticker"] = j_0[i];
-		jj["id"] = j_1[i];
-		jj["description"] = j_2[i];
-		json_data_converte.push_back(jj);
-	}
-	
-	std::cout << json_data_converte << std::endl;
 }
